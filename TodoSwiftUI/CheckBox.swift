725fed9f42248a77761720296693cbf1a98ee533
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-01-29 08:20:23 +0900

    create UserView

--- TodoSwiftUI/CheckBox.swift
+++ TodoSwiftUI/CheckBox.swift
@@ -7,15 +7,24 @@
 
 import SwiftUI
 
-struct CheckBox: View {
-//    @State var checked: Bool = false
-    @Binding var checked:Bool //初期値を設定しない場合 //@Steteとは、変数をSwiftUIで管理すること。これを使うとUI上で変更できる。
+struct CheckBox<Label>: View where Label: View{ //型バインディング
+//    @State var checked: Bool = false @Steteとは、変数をSwiftUIで管理すること。これを使うとUI上で変更できる。
+    @Binding var checked:Bool //初期値を設定しない場合
+    private var label: () -> Label
+    
+    public init(checked: Binding<Bool>,@ViewBuilder label: @escaping () -> Label){
+        self._checked = checked
+        self.label = label
+    }
     var body: some View {
         
-        Image (systemName: checked ? "checkmark.circle" : "circle")
-            .onTapGesture {
-                self.checked.toggle()
+        HStack {
+            Image (systemName: checked ? "checkmark.circle" : "circle")
+                .onTapGesture {
+                    self.checked.toggle()
             }
+            label()
+        }
 //        Toggle(isOn: $checked, label: {
 //            //バインディング構造体　toggleを変更するとcheckedに反映される。
 //            Text("チェックボックス")
@@ -26,9 +35,13 @@ struct CheckBox: View {
 struct CheckBox_Previews: PreviewProvider {
     //プレビュー画面に描画するメソッド
     static var previews: some View {
-        Group {
-            CheckBox(checked: .constant(false))
-            CheckBox(checked: .constant(true))
+        VStack {
+            CheckBox(checked: .constant(false)){
+                Text("牛乳を買う")
+            }
+            CheckBox(checked: .constant(true)){
+                Image(systemName: "hand.thumbsup")
+            }
         }
     }
 }
